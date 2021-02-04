59006bc29cb6a8b73b717eb6548a35f5eed8f2dd
DavidHunter73 <davidhequ@gmail.com>
2021-02-04 19:21:25 +0100

    InfyOm Funciona 1

---
+++ UF2/Larabel/test4v2/app/Models/Personaje.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Models;
+
+use Eloquent as Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+/**
+ * Class Personaje
+ * @package App\Models
+ * @version February 4, 2021, 3:59 pm UTC
+ *
+ * @property string $nom
+ * @property string $especie
+ */
+class Personaje extends Model
+{
+    //use SoftDeletes;
+
+    use HasFactory;
+
+    public $table = 'personajes';
+    public $timestamps = false;
+    
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+
+
+    protected $dates = ['deleted_at'];
+
+
+
+    public $fillable = [
+        'nom',
+        'especie'
+    ];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'integer',
+        'nom' => 'string',
+        'especie' => 'string'
+    ];
+
+    /**
+     * Validation rules
+     *
+     * @var array
+     */
+    public static $rules = [
+        'nom' => 'required|string',
+        'especie' => 'required|string'
+    ];
+
+    
+}
---
+++ UF2/Larabel/test4v2/resources/views/personajes/show_fields.blade.php
@@ -0,0 +1,12 @@
+<!-- Nom Field -->
+<div class="col-sm-12">
+    {!! Form::label('nom', 'Nom:') !!}
+    <p>{{ $personaje->nom }}</p>
+</div>
+
+<!-- Especie Field -->
+<div class="col-sm-12">
+    {!! Form::label('especie', 'Especie:') !!}
+    <p>{{ $personaje->especie }}</p>
+</div>
+
